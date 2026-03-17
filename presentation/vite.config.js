d62f95ad941a1c2bbde5f183e59ce07506b4f734
so2minu <israeladetola@proton.me>
2026-03-17 22:10:16 +0000

    Remove presentation and deploy workflow from repo (keeping files locally)

--- presentation/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Volume-Profile-Predictive-Model/',
-})
